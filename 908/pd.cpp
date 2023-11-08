8670758517b554e215b80794a72820c130d3f3cc
ckeisc43rd-cjtsai <jimtsai9596@gmail.com>
2023-11-08 13:29:22 +0800

    commit

--- 908/pd.cpp
+++ 908/pd.cpp
@@ -9,80 +9,29 @@ int main() {
 	while(t--){
     	int n, m;
 	    cin >> n>>m;
-    	vector<int> LIS;
 		vector<int> li(n);
-		vector<int> ind;
 		int mini=INT32_MAX, maxi=INT32_MIN, mai;
 	    for(int i = 0; i < n; ++i) {
         	int buf;
     	    cin >> buf;
 			li[i]=buf;
-			mini=min(mini, li[i]);
-			if(buf>=maxi){
-				maxi=buf;
-				mai=i;
-			}
-			
-			//st.insert(buf);
-	        if(LIS.empty() || buf > LIS.back()) {
-        	    LIS.push_back(buf);
-				ind.pb(i);
-    	    }
-	        else {
-            	auto p = lower_bound(LIS.begin(), LIS.end(), buf);
-            	*p = buf;
-				//ind[p-LIS.begin()]=i;
-        	}
-    	}
-		maxi=LIS[LIS.size()-1];
-		vector<int> sm, bg, mid;
-		for(int i=0; i<m; i++){
-			int a;cin>>a;
-			if(a>=maxi) sm.pb(a);
-			else if(a<=mini) bg.pb(a);
-			else mid.pb(a);
 		}
-		sort(sm.begin(), sm.end(), greater<int>());
-		sort(bg.begin(), bg.end(), greater<int>());
-		sort(mid.begin(), mid.end(), greater<int>());
-		vector<int> ans;
-		for(int i=0; i<=mai; i++) ans.pb(li[i]);
-		/*int sma=mid.size();
-		int lip=0, lia=0;
-		if(li[lip]<mid[lia]){
-			ans.pb(li[lip]);
-			lip++;
+	vector<int> q(m);
+	for(int i=0; i<m; i++) cin>>q[i];
+	sort(q.begin(), q.end(), greater<int>());
+	int inl=0, inq=0;
+	vector<int> ans;
+	while(inl<n){
+		if(q[inq]>li[inl]&&inq<m){
+			ans.pb(q[inq]);
+			inq++;
 		}else{
-			ans.pb(mid[lia]);
-			lia++;
-		}	
-		while(true){
-			if(lia==sma&&lip==n) break;
-			if(lia==sma){
-				ans.pb(li[lip]);
-				lip++;
-				continue;
-			}
-			if(lip==n){
-				ans.pb(mid[lia]);
-				lia++;
-				continue;
-			}
-			if(mid[lia]<ans.back()){
-				ans.pb(mid[lia]);
-				lia++;
-			}
-		//break;
-		}*/
-		mai=ind[ind.size()-1];
-		for(int x:sm) cout<<x<<' ';
-		for(int i=0; i<=mai; i++) cout<<li[i]<<' ';
-		for(int x:mid) cout<<x<<' ';
-		for(int i=mai+1; i<n; i++) cout<<li[i]<<' ';
-		for(int x:bg) cout<<x<<' ';
-		cout<<'\n';
-	}
-    return 0;
+			ans.pb(li[inl]);
+			inl++;
+		}
+	}for(int x:ans) cout<<x<<' ';
+	for(int i=inq; i<m; i++){
+		cout<<q[i]<<' ';
+	}cout<<'\n';
 }
-
-
+}
